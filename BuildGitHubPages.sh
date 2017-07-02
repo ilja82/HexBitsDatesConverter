5525e4106b2243e446a3453dbab7ae9cb04f2412
ilja <iljaerbis@gmail.com>
2017-07-02 13:55:53 +0200

    GitHub pages updated

--- BuildGitHubPages.sh
+++ BuildGitHubPages.sh
@@ -3,9 +3,8 @@
 rm -rf dist
 rm -rf docs
 
-ng build --prod -bh /HexBitsDatesConverter/
-
-mv dist docs
+ng build --prod --output-path docs --base-href HexBitsDatesConverter
+cp docs/index.html docs/404.html
 
 ls -lah docs
 read -n1 -r -p "Check if all GitHub pages are shown. Then press any key to continue..." key
